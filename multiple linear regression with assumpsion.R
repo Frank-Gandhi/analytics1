0c449324fbb7209308a6b3a6959ab1bae04e6af1
unknown <frankgandhi55@gmail.com>
2018-08-17 11:49:06 +0530

    correct A

---
+++ multiple linear regression with assumpsion.R
@@ -0,0 +1,46 @@
+#multiple linear regression
+url="https://docs.google.com/spreadsheets/d/1h7HU0X_Q4T5h5D1Q36qoK40Tplz94x_HZYHOJJC_edU/edit#gid=1595306231"
+omni=as.data.frame(gsheet::gsheet2tbl(url))
+head(omni)
+
+#predict data for different combination of price and promotion
+mlmodel1=lm(sqty~price + promotion, data=omni)
+summary(mlmodel1)
+# sqty=5837-(53*price)+(3.6*promotion)
+
+(ndata1=data.frame(price=c(65,69),promotion=c(250,300)))
+(sqty=predict(mlmodel1, newdata=ndata1,type='response'))
+cbind(ndata1,sqty)
+
+#predict data for omni
+#check assumptions for linear regression
+# 1. is there linear relationship b/w in independent and dependent variable 
+car::crPlot(mlmodel1)
+plot(mlmodel1)
+par(mfrow=c(1,1))
+plot(mlmodel1,which=1)
+#points around staight red line
+#are the regression normally distributed
+plot(density(residuals(mlmodel1)))
+
+plot(mlmodel1,which=2)
+# is their auto correlation
+car::durbinWatsonTest(mlmodel1)
+# if p value >0.05, there is auto correlation
+# is their multi collinearity
+car::vif(mlmodel1)
+
+# are their significant outliers which can attect model1
+plot(mlmodel1,4)
+
+
+#predict for new values of Iv
+(ndata1=data.frame(price=c(60:70),promotion=c(seq(250,350,by=10))))
+range(omni$price) ; range(omni$promotion)
+(sqty=predict(mlmodel1,newdata=ndata1,type='response'))
+cbind(ndata1,sqty)
+
+
+
+
+
