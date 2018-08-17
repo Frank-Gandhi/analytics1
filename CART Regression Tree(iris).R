35b0d98e2ca084c4d83965664f2ad1004d0e8b09
unknown <frankgandhi55@gmail.com>
2018-08-17 15:21:41 +0530

    correct A

--- CART Regression Tree(iris).R
+++ CART Regression Tree(iris).R
@@ -13,7 +13,10 @@ names(iris)
 
 #classification Tree
 
-
 #predict Species
 ctree=rpart(Species~.,method='class',data=iris)
-ctree=rpart(Species~Petal.Length + Petal.Width + Sepal.Length + Sepal.Width,method='class',data=iris)
+(ctree=rpart(Species~Petal.Length + Petal.Width + Sepal.Length + Sepal.Width,method='class',data=iris))
+
+rpart.plot(ctree, main='Classification Tree', nn=T, type=4, extra=104, cex=1.2)
+printcp(ctree)
+
